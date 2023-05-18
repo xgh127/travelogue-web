072cb359a915ca2c7183fb24c5b29ed36e43a664
Xu Guohong <xgh127@sjtu.edu.cn>
2023-05-18 21:59:42 +0800

    实现了一个协同过滤的函数，用一个个人新建的项目测试通过

--- src/Utils/ajax.js
+++ src/Utils/ajax.js
@@ -39,7 +39,7 @@ export const doJSONPut = async(url, json) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(json),
-        credentials: "include"
+        // credentials: "include"
     }
 
     let response = await fetch(ip + url, opts);
@@ -52,10 +52,23 @@ export const doDelete = async(url) => {
         headers: {
             Accept: 'application/json',
         },
-        credentials: "include"
+        // credentials: "include"
     }
 
     let response = await fetch(ip + url, opts);
+    let responseJSON = await response.json();
+    return responseJSON;
+}
+export const doRecommentGet = async(url) => {
+    let opts = {
+        method: "GET",
+        headers: {
+            Accept: 'application/json',
+        },
+        // credentials: "include"
+    }
+
+    let response = await fetch(url, opts);
     let responseJSON = await response.json();
     return responseJSON;
 }
